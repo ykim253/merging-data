070a52fb1ecc20415551da1b4fbdf9e0113b39d3
Dave Stearns <dlsinfo@uw.edu>
2014-09-13 12:09:22 -0700

    initial files

---
+++ js/app.js
@@ -0,0 +1,7 @@
+/* 
+    app.js
+    Angular application for the address book challenge
+
+    Add code here to create a new Angular application and controller.
+    The array of employee objects is already in a global variable named 'pawneeEmployees'
+*/
---
+++ js/employees.js
@@ -0,0 +1,125 @@
+var pawneeEmployees = [
+    {
+        firstName: 'Leslie',
+        lastName: 'Knope',
+        title: 'Deputy Director',
+        department: 'Parks',
+        phone: '555-1111',
+        email: 'knope@pawnee.ia.gov',
+        picUrl: 'img/knope.jpg'
+    },
+    {
+        firstName: 'Tom',
+        lastName: 'Haverford',
+        title: 'Administrator',
+        department: 'Parks',
+        phone: '555-1112',
+        email: 'haverford@pawnee.ia.gov',
+        picUrl: 'img/haverford.jpg'
+    },
+    {
+        firstName: 'April',
+        lastName: 'Ludgate-Dwyer',
+        title: 'Administrative Assistant',
+        department: 'Parks',
+        phone: '555-1113',
+        email: 'ludgate@pawnee.ia.gov',
+        picUrl: 'img/ludgate.jpg'
+    },
+    {
+        firstName: 'Meagle',
+        lastName: 'Donna',
+        title: 'Permits Secretary',
+        department: 'Parks',
+        phone: '555-1114',
+        email: 'meagle@pawnee.ia.gov',
+        picUrl: 'img/meagle.jpg'
+    },
+    {
+        firstName: 'Jerry',
+        lastName: 'Gergich',
+        title: 'Office Manager',
+        department: 'Parks',
+        phone: '555-1115',
+        email: 'gergich@pawnee.ia.gov',
+        picUrl: 'img/gergich.jpg'
+    },
+    {
+        firstName: 'Andy',
+        lastName: 'Dwyer',
+        title: 'Shoe Shiner',
+        department: 'Facilities',
+        email: 'radical@mouserat.com',
+        picUrl: 'img/dwyer.jpg'
+    },
+    {
+        firstName: 'Bill',
+        lastName: 'Dexhart',
+        title: 'Councilman',
+        department: 'Council',
+        phone: '555-2111',
+        email: 'dexhart@pawnee.ia.gov',
+        picUrl: 'img/dexhart.jpg'
+    },
+    {
+        firstName: 'Jeremy',
+        lastName: 'Jamm',
+        title: 'Councilman',
+        department: 'Council',
+        phone: '555-2112',
+        email: 'jamm@pawnee.ia.gov',
+        picUrl: 'img/jamm.jpg'
+    },
+    {
+        firstName: 'Knope',
+        lastName: 'Leslie',
+        title: 'Councilwoman',
+        department: 'Council',
+        phone: '555-2113',
+        email: 'knope@pawnee.ia.gov',
+        picUrl: 'img/knope-council.jpg'
+    },
+    {
+        firstName: 'Fielding',
+        lastName: 'Milton',
+        title: 'Councilman',
+        department: 'Council',
+        phone: '555-2114',
+        email: 'milton@pawnee.ia.gov',
+        picUrl: 'img/milton.jpg'
+    },
+    {
+        firstName: 'Douglass',
+        lastName: 'Howser',
+        title: 'Councilman',
+        department: 'Council',
+        phone: '555-2115',
+        email: 'howser@pawnee.ia.gov',
+        picUrl: 'img/howser.jpg'
+    },
+    {
+        firstName: 'Ben',
+        lastName: 'Wyatt',
+        title: 'Fiscal Advisor',
+        department: 'Finance',
+        phone: '555-3111',
+        email: 'wyatt@pawnee.ia.gov',
+        picUrl: 'img/wyatt.jpg'
+    },
+    {
+        firstName: 'Chris',
+        lastName: 'Traeger',
+        title: 'Auditor',
+        department: 'Finance',
+        phone: '555-3112',
+        email: 'traeger@pawnee.ia.gov',
+        picUrl: 'img/traeger.jpg'
+    },
+    {
+        firstName: "Li'l",
+        lastName: 'Sebastian',
+        title: 'Mascot',
+        department: 'Entertainment',
+        picUrl: 'img/sebastian.jpg'
+    }
+];
